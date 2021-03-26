dbcdf90ba3e61b81d4239c806f64572301aa7b47
chinch2 <johao5991@gmail.com>
2021-03-26 19:43:58 -0400

    Front done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        width: {
+            '96': '24rem',
+        }
+    },
   },
   variants: {
     extend: {},
